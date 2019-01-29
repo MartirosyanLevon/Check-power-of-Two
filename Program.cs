6b8cfcae7c3ceb9262a0c8559ff9f5a2b3698e25
Lev# <mr.lmartirosyan888@gmail.com>
2019-01-29 12:45:30 +0400

    relise one

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check__power_of_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            Console.WriteLine("Write a numbre for check:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            s = x & (x - 1);
+            if (s == 0)
+            {
+                Console.WriteLine("The numbre is = power of 2");
+            }
+            else
+            {
+                Console.WriteLine("The numbre in not = power of two 2");
+            }
+            Console.ReadKey();
+        }
+    }
+}
